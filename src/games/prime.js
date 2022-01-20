5739cca92bad8d079bf856dd5bd31feaad3d5e14
reggullus <Reggullus@yandex.ru>
2022-01-20 18:57:06 +0500

    fix file

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,6 +3,9 @@ import gameLogic from '../index.js';
 
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (num) => {
+  if (num === 1) {
+    return false;
+  }
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
       return false;
